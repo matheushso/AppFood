3917c7aad8de4d5dd55eccc51ab30b93b91bece2
Matheus Oliveira <matheushso99@hotmail.com>
2020-09-25 01:04:54 -0300

    Telas de aplicativo de delivery - Prova PDM ESOFT6S

---
+++ Monitora/app/src/main/java/com/example/monitora/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.monitora
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        mudarParaActivityDuvida()
+    }
+
+    private fun mudarParaActivityDuvida() {
+        val intent = Intent(this, FoodActivity::class.java)
+
+        Handler().postDelayed({
+            intent.change()
+        }, 5000)
+    }
+
+    fun Intent.change() {
+        startActivity(this)
+        finish()
+    }
+}
